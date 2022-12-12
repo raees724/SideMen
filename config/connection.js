3ee42fdcb9917ae023e00b2993ef11e380d2d8b4
raees724 <ahmedraees724@gmail.com>
2022-12-12 14:35:20 +0530

    third commit

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,10 @@ const state = {
 }
 
 module.exports.connect=function(done) {
-    const url='mongodb://localhost:27017';
+    // const url='mongodb://localhost:27017';
+    const url='mongodb+srv://raeesahmed:724@cluster0.6gbj8pv.mongodb.net';
+    
+    
     const dbname='sidemen'
 
     mongoClient.connect(url, (err, data) => {
