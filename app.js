c54ec1bd127ed64bf3b21565b691b29de9533f9b
raees724 <ahmedraees724@gmail.com>
2023-05-10 17:30:57 +0530

    port changed

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT)
+// app.listen(process.env.PORT)
+app.listen(process.env.PORT || 8000)
 
 module.exports = app;
